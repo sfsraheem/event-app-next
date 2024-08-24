ecb7c41e79094430c9651227c2341a78542af481
sfsraheem <116721524+sfsraheem@users.noreply.github.com>
2024-08-24 13:41:10 +0500

    Footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Eventify"
+            width={128}
+            height={38}
+          />
+        </Link>
+        <p>2024 Eventify. All rights reserved.</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
