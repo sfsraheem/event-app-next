e9fbd756b870362e0eb92aa7dda869ca9db23d95
sfsraheem <sfsraheem222@gmail.com>
2024-08-31 15:52:26 +0500

    key issue fixed

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -56,7 +56,7 @@ const CategoryFilter = () => {
         <SelectItem value="All" className="select-item p-regular-14">All</SelectItem>
 
         {categories.map((category) => (
-          <SelectItem value={category.name} key={category._id} className="select-item p-regular-14">
+          <SelectItem value={category.name} key={category._id?.toString()} className="select-item p-regular-14">
             {category.name}
           </SelectItem>
         ))}
